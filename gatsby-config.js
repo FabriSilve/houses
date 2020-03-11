33a160e874e9459475415710cb2ae83cc8967253
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-03-11 19:36:48 +0100

    add gatsby pugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,19 @@
 module.exports = {
   pathPrefix: "/houses",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Houses`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `pages`,
+        path: `${__dirname}/content`,
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
